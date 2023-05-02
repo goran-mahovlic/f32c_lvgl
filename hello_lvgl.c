9d41cfe429b1944d544747964d02b1d91cec4032
Goran Mahovlić <goran.mahovlic@gmail.com>
2023-05-03 01:05:50 +0200

    Label is now shown - but disp_flush_cb is wrong

--- hello_lvgl.c
+++ hello_lvgl.c
@@ -12,7 +12,11 @@
 volatile static lv_disp_draw_buf_t draw_buf;
 
 volatile static lv_color_t *buf1 = NULL;
-volatile static lv_color_t *buf2 = NULL;
+#define F32C_VIDEOWIDTH 512
+#define F32C_VIDEOHEIGHT 288
+#define BLACK 0x0000
+
+//volatile static lv_color_t *buf2 = NULL;
 
 static uint32_t tsc_hi, tsc_lo;
 int tmp, freq_khz;
@@ -89,23 +93,29 @@ void disp_driver_rounder(lv_disp_drv_t * disp_drv, lv_area_t * area)
 */
 
 void disp_driver_set_px(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
-    lv_color_t * color_p, lv_opa_t opa) 
+    lv_color_t color_p, lv_opa_t opa) 
 {
-    fb_plot(x, y, (int)color_p);
+
+  if ((x < 0) || (x >= F32C_VIDEOWIDTH) || (y < 0) || (y >= F32C_VIDEOHEIGHT))
+    return;
+
+  //fb_plot(x,y,color);
+  fb_plot(x-1, y-1, (int)&color_p);
 }
 
 void disp_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
 {
     /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
+
     int32_t x, y;
-    //int color = (int)color_p;
     for(y = area->y1; y <= area->y2; y++) {
         for(x = area->x1; x <= area->x2; x++) {
-            fb_plot(x, y, (int)color_p);
+            // This line needs fix
+            //fb_plot(x-1, y-1, (int)&color_p);
             color_p++;
         }
     }
-
+  
     /* IMPORTANT!!!
      * Inform the graphics library that you are ready with the flushing*/
     lv_disp_flush_ready(disp_drv);
@@ -120,18 +130,18 @@ main(void)
     fb_set_mode(0);
     // Test f32c text to framebuffer
     fb_text(100, 100, "No LVGL FB test", 0x0000, 0xffff, 3);
-    delay(2000);
+    delay(1000);
 
     lv_init();
 
-    buf1 = malloc(512 * 288 * 16);
-    buf2 = malloc(512 * 288 * 16);
+    buf1 = malloc(F32C_VIDEOWIDTH * F32C_VIDEOHEIGHT * 16);
+    //buf2 = malloc(512 * 288 * 16);
 
     lv_log_register_print_cb( printf ); /* register print function for debugging */
 
     /* static lv_color_t * */
 
-    lv_disp_draw_buf_init( &draw_buf, buf1, buf2, 512 * 288 * 16);
+    lv_disp_draw_buf_init( &draw_buf, buf1, NULL, F32C_VIDEOWIDTH * F32C_VIDEOHEIGHT * 16);
     /*Initialize the display*/
     static lv_disp_drv_t disp_drv;
 
@@ -140,36 +150,36 @@ main(void)
     disp_drv.flush_cb = disp_flush_cb;
 
     /*Set the resolution of the display*/
-    disp_drv.hor_res = 512;
-    disp_drv.ver_res = 288;
+    disp_drv.hor_res = F32C_VIDEOWIDTH;
+    disp_drv.ver_res = F32C_VIDEOHEIGHT;
 
 //    disp_drv.rounder_cb = disp_driver_rounder;
     disp_drv.set_px_cb  = disp_driver_set_px;
     disp_drv.draw_buf = &draw_buf;
-    disp_drv.full_refresh = true;
+//    disp_drv.full_refresh = true;
 
     lv_disp_drv_register( &disp_drv );
 
 #ifdef NODEMO
     /*Change the active screen's background color*/
-    delay(1000);
+    delay(200);
     printf("Setting background color\n");
-    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0xffff), LV_PART_MAIN);
-    delay(1000);
+    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x0000), LV_PART_MAIN);
+    delay(200);
     /*Create a white label, set its text and align it to the center*/
     lv_obj_t * label = lv_label_create(lv_scr_act());
-    delay(1000);
+    delay(200);
     printf("Creating label\n");    
-    lv_label_set_text(label, "Hello world");
-    lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0x0000), LV_PART_MAIN);
-    delay(1000);
+    lv_label_set_text(label, "LVGL LABEL TEXT >> disp_flush_cb needs to be fixed");
+    lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffff), LV_PART_MAIN);
+    delay(200);
     printf("Align label\n");    
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 #else
-    delay(1000);
+    delay(200);
     printf("Setting up demo widgets\n");    
     lv_demo_widgets();
-    delay(1000);
+    delay(200);
     printf("Widgets setup done\n");
 #endif
 
@@ -177,6 +187,6 @@ main(void)
         delay(500);
         printf("In the loop\n");
         printf("Millis: %i\n", millis());
-        lv_timer_handler(); /* let the GUI do its work */
+        lv_timer_handler(); /* let the GUI do its work */ 
     }
 }

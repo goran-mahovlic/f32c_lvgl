ecf6b05e2dfd327bca1816d02c77b80c05476377
Goran Mahovlić <goran.mahovlic@gmail.com>
2023-05-04 19:48:40 +0200

    Adding C2 sample

--- hello_lvgl.cpp
+++ hello_lvgl.cpp
@@ -10,38 +10,33 @@ extern "C" {
 //#include <examples/lv_examples.h>
 }
 
-#include "Compositing/Compositing.h"
-
+#include <Compositing.h>
 #define SPRITE_MAX 10
+Compositing c2;
 
-//                            RRGGBB
-#define C2_WHITE  RGB2PIXEL(0xFFFFFF)
-#define C2_GREEN  RGB2PIXEL(0x002200)
-#define C2_ORANGE RGB2PIXEL(0xFF7F00)
-#define C2_BLUE   RGB2PIXEL(0x4080FF)
+//Define Visuals
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define ANZCOL 256
+#define emu_set_pix(x,y,c) bitmap[x + SCREEN_WIDTH*y] = color_map[c]
 
-//#define RESOLUTION_X VGA_X_MAX
-// 624x480 16bpp is max useable until SDRAM burst is supported
-#define RESOLUTION_X 624
-#define RESOLUTION_Y 480
-#define OFF_SCREEN_Y 2000
+// crude malloc()
+pixel_t *bitmap = (pixel_t *)0x80080000;
+pixel_t color_map[ANZCOL];
 
 #define F_CPU 100000000
 #define NODEMO
+//#define SPINNER
 
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t *buf1 = NULL;
+static lv_color_t *buf1 = (lv_color_t *)0x80080000;
 
-Compositing c2;
-
-#define F32C_VIDEOWIDTH RESOLUTION_X
-#define F32C_VIDEOHEIGHT RESOLUTION_Y
-#define BLACK 0x0000
+#define F32C_VIDEOWIDTH SCREEN_WIDTH
+#define F32C_VIDEOHEIGHT SCREEN_HEIGHT
+//#define BLACK 0x0000
 
 // uncomment if you are using LOG
 //#define DEBUG
-
-//volatile static lv_color_t *buf2 = NULL;
 
 static uint32_t tsc_hi, tsc_lo;
 int tmp, freq_khz;
@@ -75,12 +70,11 @@ update_tsc(void)
 
     RDTSC(tsc);
 
-    /* check for 32-bit overflow */
+    // check for 32-bit overflow
     if (tsc < tsc_lo)
         tsc_hi++;
     tsc_lo = tsc;
 }
-
 
 uint32_t
 millis(void)
@@ -95,27 +89,9 @@ millis(void)
     return((tsc64 * 1000) / F_CPU);
 }
 
-
 void disp_driver_rounder(lv_disp_drv_t * disp_drv, lv_area_t * area)
 {
-  /* Update the areas as needed. Can be only larger.
-   * For example to always have lines 8 px height:*/
-   area->y1 = area->y1 & 0x07;
-   area->y2 = (area->y2 & 0x07) + 8;
 }
-
-/*
-void disp_driver_rounder(lv_disp_drv_t * disp_drv, lv_area_t * area)
-{
-    uint8_t hor_max = disp_drv->hor_res;
-    uint8_t ver_max = disp_drv->ver_res;
-
-    area->x1 = 0;
-    area->y1 = 0;
-    area->x2 = hor_max - 1;
-    area->y2 = ver_max - 1;
-}
-*/
 
 void disp_driver_set_px(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
     lv_color_t color_p, lv_opa_t opa) 
@@ -123,9 +99,7 @@ void disp_driver_set_px(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_
 
   if ((x < 0) || (x >= F32C_VIDEOWIDTH) || (y < 0) || (y >= F32C_VIDEOHEIGHT))
     return;
-
-  //fb_plot(x,y,color);
-  fb_plot(x-1, y-1, (int)&color_p);
+    emu_set_pix(x,y,(int)&color_p); // Needs to be removed
 }
 
 void disp_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
@@ -133,14 +107,18 @@ void disp_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t 
     /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
 
     int32_t x, y;
-    for(y = area->y1; y <= area->y2; y++) {
-        for(x = area->x1; x <= area->x2; x++) {
-            // This line needs fix
-            //fb_plot(x-1, y-1, (int)&color_p);
-            color_p++;
-        }
+
+  // set color map
+  //for(int i = 0; i < ANZCOL; i++)
+  //  color_map[i] = RGB2PIXEL((int)&color_p);
+
+  for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++) {
+        //bitmap[i] = 0x00;  // works - sets screen to black
+        //emu_set_pix(x,y,(int)&color_p);
+        emu_set_pix(x,y,uint16_t(RGB2PIXEL((int)&color_p)));
+        color_p++;
     }
-  
+   // c2.sprite_refresh();
     /* IMPORTANT!!!
      * Inform the graphics library that you are ready with the flushing*/
     lv_disp_flush_ready(disp_drv);
@@ -150,29 +128,25 @@ void
 main(void)
 {
 
-    printf("Hello, f32c/%s world!\n", arch);
+  delay(1000);
+  printf("Hello, f32c/%s world!\n", arch);
 
+  buf1 = (lv_color_t *) malloc(F32C_VIDEOWIDTH*F32C_VIDEOHEIGHT*sizeof(lv_color_t));
+  bitmap = (pixel_t *) buf1; // alloc memory for video
   c2.init();
   c2.alloc_sprites(SPRITE_MAX);
-  
-  // sprite 0: ball
-  c2.sprite_fill_rect(100, 100, C2_ORANGE);
-  // draw them all
-  c2.sprite_refresh();
-  *c2.cntrl_reg = 0b11000000; // enable video, yes bitmap, no text mode, no cursor
-  
-  //expectBY = expectB();
+  //bitmap = (pixel_t *) malloc(SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(pixel_t)); // alloc memory for video
+  c2.sprite_from_bitmap(SCREEN_WIDTH, SCREEN_HEIGHT, bitmap); // create c2 sprite
+//  c2.sprite_refresh(); // show it on screen
+  *c2.cntrl_reg = 0b11000000; // vgatextmode: enable video, yes bitmap, no text mode, no cursor
+   c2.sprite_refresh();
 
-  delay(5000);
-//    fb_set_mode(1);
-    // Test f32c text to framebuffer
-//    fb_text(100, 100, "No LVGL FB test", 0x0000, 0xffff, 3);
-//    delay(1000);
+  for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(lv_color_t); i++) {
+        bitmap[i] = 0x00;  // set screen to white
+    }
+  //delay(2000);
 
     lv_init();
-
-    buf1 = (lv_color_t *) malloc(F32C_VIDEOWIDTH * F32C_VIDEOHEIGHT * 16);
-    //buf2 = malloc(512 * 288 * 16);
 
     #ifdef DEBUG
         lv_log_register_print_cb( printf ); /* register print function for debugging */
@@ -180,7 +154,7 @@ main(void)
 
     /* static lv_color_t * */
 
-    lv_disp_draw_buf_init( &draw_buf, buf1, NULL, F32C_VIDEOWIDTH * F32C_VIDEOHEIGHT * 16);
+    lv_disp_draw_buf_init( &draw_buf, buf1, NULL, F32C_VIDEOWIDTH * F32C_VIDEOHEIGHT * sizeof(lv_color_t));
     /*Initialize the display*/
     static lv_disp_drv_t disp_drv;
 
@@ -195,25 +169,39 @@ main(void)
 //    disp_drv.rounder_cb = disp_driver_rounder;
     disp_drv.set_px_cb  = disp_driver_set_px;
     disp_drv.draw_buf = &draw_buf;
-//    disp_drv.full_refresh = true;
+    //disp_drv.full_refresh = true;
 
     lv_disp_drv_register( &disp_drv );
 
 #ifdef NODEMO
     /*Change the active screen's background color*/
-    delay(200);
+    //delay(200);
     printf("Setting background color\n");
-    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x0000), LV_PART_MAIN);
-    delay(200);
+    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x00), LV_PART_MAIN);
+    //delay(200);
     /*Create a white label, set its text and align it to the center*/
     lv_obj_t * label = lv_label_create(lv_scr_act());
-    delay(200);
+    //delay(200);
     printf("Creating label\n");    
     lv_label_set_text(label, "LVGL LABEL TEXT >> disp_flush_cb needs to be fixed");
     lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffff), LV_PART_MAIN);
-    delay(200);
-    printf("Align label\n");    
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    //delay(200);
+    printf("Align label\n");
+    lv_obj_set_x(label, 100) ;
+    lv_obj_set_y(label, 100) ;
+    #ifdef SPINNER
+        printf("Create spinner\n");    
+        /*Create a spinner*/
+        lv_obj_t * spinner = lv_spinner_create(lv_scr_act(), 1000, 60);
+        lv_obj_set_size(spinner, 100, 100);
+        printf("Center spinner\n");    
+        lv_obj_center(spinner);
+        printf("Set spinner X Y \n");      
+        lv_obj_set_x(spinner, 200) ;
+        lv_obj_set_y(spinner, 200) ;
+        printf("Init done!\n"); 
+    #endif
+    //lv_obj_align(label, LV_ALIGN_CENTER, 0, 100);
 #else
     delay(200);
     printf("Setting up demo widgets\n");    
@@ -221,11 +209,10 @@ main(void)
     delay(200);
     printf("Widgets setup done\n");
 #endif
-
     while(1){
-        delay(500);
-        printf("In the loop\n");
-        printf("Millis: %i\n", millis());
-        lv_timer_handler(); /* let the GUI do its work */ 
+        delay(100);
+        //printf("In the loop\n");
+        //printf("Millis: %i\n", millis());
+        lv_timer_handler(); /* let the GUI do its work */
     }
 }
